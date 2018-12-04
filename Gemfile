193b8320d10ef5daadd64246d7fea494775c499e
ravindra rawat <ravinrawat.wd@gmail.com>
2018-12-04 15:37:31 -0700

    exe 2

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+ruby "2.3.7"
+
 gem 'pry' # for debugging
 gem 'pg'
 gem 'activerecord'
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+# puts Store.count
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.name = "Calgary"
+@store1.save
+
+# update(name: 'Dave')
+
 # Your code goes here ...
